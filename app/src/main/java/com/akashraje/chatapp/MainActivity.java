06f8044257db0eb1560071147ec4307cbb8f0955
Akash Raje <akash.raje@gmail.com>
2017-07-02 17:42:14 +0530

    Firebase and menu options added

--- app/src/main/java/com/akashraje/chatapp/MainActivity.java
+++ app/src/main/java/com/akashraje/chatapp/MainActivity.java
@@ -1,7 +1,16 @@
 package com.akashraje.chatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.akashraje.chatapp.Utils.Constants;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(),
+                    Constants.SIGN_IN_REQUEST_CODE);
+        } else {
+
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Constants.SIGN_IN_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this,
+                        "Welcome " + FirebaseAuth.getInstance()
+                                .getCurrentUser()
+                                .getDisplayName(),
+                        Toast.LENGTH_LONG)
+                        .show();
+
+
+
+
+            } else {
+                Toast.makeText(this,
+                        "Unable to sign you in.",
+                        Toast.LENGTH_LONG)
+                        .show();
+            }
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/akashraje/chatapp/Utils/Constants.java
@@ -0,0 +1,9 @@
+package com.akashraje.chatapp.Utils;
+
+/**
+ * Created by akashraje on 02/07/17.
+ */
+
+public class Constants {
+    public static int SIGN_IN_REQUEST_CODE = 1;
+}
